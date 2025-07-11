3770126f929a573d997d1010f7ab58c7a9fed69c
pragma <pragmaticTNT@gmail.com>
2025-07-11 14:57:44 +0000

    Added github credential management

--- runpod.sh
+++ runpod.sh
@@ -6,8 +6,10 @@ uv --version # Ensure the script is run from the correct directory
 
 # Create the virtual environment and install dependencies
 git clone https://github.com/pragmaticTNT/latentqa.git
+git config --global user.email "pragmaticTNT@gmail.com"
+git config --global user.name "pragma"
+git config --global credential.helper store
+
 uv venv
 source .venv/bin/activate
 uv pip install -r requirements.txt
-
-# Setup github ssh key
